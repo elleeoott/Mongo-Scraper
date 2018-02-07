7de2486f85bdff03cfde5a6bcc1ab67db391264c
Evan Darst <evan.l.darst@gmail.com>
2018-02-07 07:06:26 -0500

    pseudo for main js

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+//function that performs the scrape
+
+//using an AJAX call, delete an article from our DB or throw an error
+
+//using an AJAX call, use put to save an article to our DB or throw an error
+
+//using an AJAX call, use get to retreive comments from our DB or throw an error
+
+//
